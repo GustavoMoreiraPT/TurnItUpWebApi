dc81310e445af558bce719d31f26c527093c6cad
Gustavo Costa Araújo Moreira <gustavo1995.2012@hotmail.com>
2019-01-30 17:17:43 +0000

    Add some logic related to refresh tokens

--- 1-Presentation/TurnItUpWebApi/Controllers/AccountsController.cs
+++ 1-Presentation/TurnItUpWebApi/Controllers/AccountsController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Security.Claims;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using Application.Dto.Users;
@@ -106,6 +107,42 @@ namespace TurnItUpWebApi.Controllers
 			return this.BadRequest();
 		}
 
+		[HttpPost]
+		public IActionResult Refresh(string token, string refreshToken)
+		{
+			var principal = GetPrincipalFromExpiredToken(token);
+			var username = principal.Identity.Name;
+			var savedRefreshToken = GetRefreshToken(username); //retrieve the refresh token from a data store
+			if (savedRefreshToken != refreshToken)
+				throw new SecurityTokenException("Invalid refresh token");
+
+			var newJwtToken = GenerateJwtToken(null, null, null, principal.Claims.ToList());
+			var newRefreshToken = GenerateRefreshToken();
+			DeleteRefreshToken(username, refreshToken);
+			SaveRefreshToken(username, newRefreshToken);
+
+			return new ObjectResult(new
+			{
+				token = newJwtToken,
+				refreshToken = newRefreshToken
+			});
+		}
+
+		private void SaveRefreshToken(string username, string newRefreshToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		private void DeleteRefreshToken(string username, string refreshToken)
+		{
+			throw new NotImplementedException();
+		}
+
+		private object GetRefreshToken(string username)
+		{
+			throw new NotImplementedException();
+		}
+
 		[HttpGet]
 		[Route("protected")]
 		[Authorize]
@@ -148,6 +185,37 @@ namespace TurnItUpWebApi.Controllers
 			);
 
 			return new JwtSecurityTokenHandler().WriteToken(token);
+		}
+
+		private ClaimsPrincipal GetPrincipalFromExpiredToken(string token)
+		{
+			var tokenValidationParameters = new TokenValidationParameters
+			{
+				ValidateAudience = false, //you might want to validate the audience and issuer depending on your use case
+				ValidateIssuer = false,
+				ValidateIssuerSigningKey = true,
+				IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("the server key used to sign the JWT token is here, use more than 16 chars")),
+				ValidateLifetime = false //here we are saying that we don't care about the token's expiration date
+			};
+
+			var tokenHandler = new JwtSecurityTokenHandler();
+			SecurityToken securityToken;
+			var principal = tokenHandler.ValidateToken(token, tokenValidationParameters, out securityToken);
+			var jwtSecurityToken = securityToken as JwtSecurityToken;
+			if (jwtSecurityToken == null || !jwtSecurityToken.Header.Alg.Equals(SecurityAlgorithms.HmacSha256, StringComparison.InvariantCultureIgnoreCase))
+				throw new SecurityTokenException("Invalid token");
+
+			return principal;
+		}
+
+		private string GenerateRefreshToken()
+		{
+			var randomNumber = new byte[32];
+			using (var rng = RandomNumberGenerator.Create())
+			{
+				rng.GetBytes(randomNumber);
+				return Convert.ToBase64String(randomNumber);
+			}
 		}
 
 		private List<Claim> AddRolesClaims(List<string> roleNamesList)
