126e9776d814983c8aca3153c3cf1822831e4de4
Gustavo Costa Araújo Moreira <gustavo1995.2012@hotmail.com>
2019-05-15 17:01:29 +0100

    revert program

--- 1-Presentation/TurnItUpWebApi/Program.cs
+++ 1-Presentation/TurnItUpWebApi/Program.cs
@@ -1,21 +1,43 @@
 ﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Serilog;
+using Serilog.Events;
+using System;
 using System.IO;
 
 namespace TurnItUpWebApi
 {
-	public class Program
-	{
-		public static void Main(string[] args)
-		{
-			CreateWebHostBuilder(args).Build().Run();
-		}
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+             .MinimumLevel.Information()
+             .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+             .Enrich.FromLogContext()
+             .WriteTo.Console()
+             .CreateLogger();
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
-                .UseStartup<Startup>();
+            try
+            {
+                Log.Information("Starting web host");
+                WebHostExtensions.Run(Program.BuildWebHost(args));
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "Host terminated unexpectedly");
+                return 1;
+            }
+            finally
+            {
+                Log.CloseAndFlush();
+            }
+        }
+
+        public static IWebHost BuildWebHost(string[] args)
+        {
+            return SerilogWebHostBuilderExtensions.UseSerilog(WebHostBuilderExtensions.UseStartup<Startup>(WebHost.CreateDefaultBuilder(args)), (ILogger)null, false).Build();
+        }
     }
 }
