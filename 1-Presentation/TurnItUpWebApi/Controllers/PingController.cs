cc140620c361a0ec1984aa0c30d714fdff5469b0
Gustavo Costa Araújo Moreira <gustavo1995.2012@hotmail.com>
2019-05-15 14:34:32 +0100

    ping working

--- 1-Presentation/TurnItUpWebApi/Controllers/PingController.cs
+++ 1-Presentation/TurnItUpWebApi/Controllers/PingController.cs
@@ -1,28 +1,28 @@
-﻿//using Microsoft.AspNetCore.Authorization;
-//using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
-//namespace TurnItUpWebApi.Controllers
-//{
-//	[Route("v1/ping")]
-//	public class PingController : Controller
-//	{
-//		public PingController()
-//		{
+namespace TurnItUpWebApi.Controllers
+{
+    [Route("v1/ping")]
+    public class PingController : Controller
+    {
+        public PingController()
+        {
 
-//		}
+        }
 
-//		[HttpGet]
-//		public IActionResult GetPing()
-//		{
-//			return new OkObjectResult("Ping");
-//		}
+        [HttpGet]
+        public IActionResult GetPing()
+        {
+            return new OkObjectResult("Ping");
+        }
 
-//		[HttpGet]
-//		[Route("pong")]
-//		public IActionResult GetPong()
-//		{
-//			var x = this.HttpContext;
-//			return new OkObjectResult("Pong");
-//		}
-//	}
-//}
+        [HttpGet]
+        [Route("pong")]
+        public IActionResult GetPong()
+        {
+            var x = this.HttpContext;
+            return new OkObjectResult("Pong");
+        }
+    }
+}
