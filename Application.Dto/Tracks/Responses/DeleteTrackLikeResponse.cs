6ffc667d9f23c591146267b4cf2ee9da627f3b48
Gustavo Moreira <gustavo1995.2012@hotmail.com>
2019-06-04 19:14:11 +0100

    Added endpoint to delete a previous like

---
+++ Application.Dto/Tracks/Responses/DeleteTrackLikeResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Dto.Tracks.Responses
+{
+    public class DeleteTrackLikeResponse
+    {
+        public bool IsDeleted { get; set; }
+
+        public bool IsTrackFound { get; set; }
+    }
+}
