b0d878bee080d82cd888ee5ab1156d50fed2a154
Gustavo Moreira <gustavo1995.2012@hotmail.com>
2019-07-13 16:55:25 +0100

    seed values for roles and genres

--- 08-GenresAndRolesSeed.sql
+++ 08-GenresAndRolesSeed.sql
@@ -1,1 +1,31 @@
-﻿
+﻿insert into webapijwt.groupsofgenrers values (null, 'Trance');
+insert into webapijwt.groupsofgenrers values (null, 'House');
+insert into webapijwt.groupsofgenrers values (null, 'Techno');
+
+insert into webapijwt.languagegenres values (null, 'Trance', 'en', 1, 1);
+insert into webapijwt.languagegenres values (null, 'Trance Portugues', 'pt', 1, 1);
+insert into webapijwt.languagegenres values (null, 'Trance Frances', 'fr', 1, 1);
+insert into webapijwt.languagegenres values (null, 'House', 'en', 2, 2);
+insert into webapijwt.languagegenres values (null, 'House Portugues', 'pt', 2, 2);
+insert into webapijwt.languagegenres values (null, 'House Frances', 'fr', 2, 2);
+insert into webapijwt.languagegenres values (null, 'Techno', 'en', 3, 3);
+insert into webapijwt.languagegenres values (null, 'Techno Portugues', 'pt', 3, 3);
+insert into webapijwt.languagegenres values (null, 'Techo Frances', 'fr', 3, 3);
+
+
+--------------------------------------------------------------------------------------
+
+insert into webapijwt.groupsofroles values (null, 'DJ');
+insert into webapijwt.groupsofroles values (null, 'Guitar Player');
+insert into webapijwt.groupsofroles values (null, 'Producer');
+
+insert into webapijwt.languagegenres values (null, 'DJ', 'en', 1, 1);
+insert into webapijwt.languagegenres values (null, 'DJ Portugues', 'pt', 1, 1);
+insert into webapijwt.languagegenres values (null, 'DJ Frances', 'fr', 1, 1);
+insert into webapijwt.languagegenres values (null, 'Guitar Player', 'en', 2, 2);
+insert into webapijwt.languagegenres values (null, 'Guitar Player Portugues', 'pt', 2, 2);
+insert into webapijwt.languagegenres values (null, 'Guitar Player Frances', 'fr', 2, 2);
+insert into webapijwt.languagegenres values (null, 'Producer', 'en', 3, 3);
+insert into webapijwt.languagegenres values (null, 'Producer Portugues', 'pt', 3, 3);
+insert into webapijwt.languagegenres values (null, 'Producer Frances', 'fr', 3, 3);
+
